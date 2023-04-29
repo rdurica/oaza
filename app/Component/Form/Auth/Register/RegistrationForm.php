f1a4c1b0f88146b089f3714d5489673509f1382d
Robert Durica <r.durica@gmail.com>
2023-04-29 21:38:03 +0200

    Changed: Registration form rules & conditions

--- app/Component/Form/Auth/Register/RegistrationForm.php
+++ app/Component/Form/Auth/Register/RegistrationForm.php
@@ -34,16 +34,18 @@ class RegistrationForm extends Component
             ->setHtmlAttribute('placeholder', $this->translator->trans('user.nameSurname'))
             ->setMaxLength(30)
             ->setRequired();
-        $form->addInteger('telephone', $this->translator->trans('user.telephone'))
+        $form->addText('telephone', $this->translator->trans('user.telephone'))
             ->setHtmlAttribute('placeholder', $this->translator->trans('user.telephone'))
             ->setRequired()
-            ->setMaxLength(13);
+            ->addRule($form::Pattern, $this->translator->trans('flash.telephoneFormat'), '^(2[0-9]{2}|3[0-9]{2}|4[0-9]{2}|5[0-9]{2}|72[0-9]|73[0-9]|77[0-9]|60[1-8]|56[0-9]|70[2-5]|79[0-9])[0-9]{3}[0-9]{3}$')
+            ->setMaxLength(9);
         $form->addPassword('password', $this->translator->trans('user.password'))
             ->setHtmlAttribute('placeholder', $this->translator->trans('user.password'))
+            ->addRule(NetteForm::MIN_LENGTH, $this->translator->trans('flash.minPasswordLength'), 8)
             ->setRequired();
         $form->addPassword('passwordVerify', $this->translator->trans('user.passwordVerify'))
             ->setHtmlAttribute('placeholder', $this->translator->trans('user.passwordVerify'))
-            ->addRule(NetteForm::EQUAL, 'flash.passwordsDoNotMatch', $form['password'])
+            ->addRule(NetteForm::EQUAL, $this->translator->trans('flash.passwordsDoNotMatch'), $form['password'])
             ->setOmitted()
             ->setRequired();
         $form->addCheckbox('licence', '')->setRequired();
@@ -60,7 +62,7 @@ class RegistrationForm extends Component
     #[NoReturn] public function onFormSuccess(Form $form, ArrayHash $values): void
     {
         try {
-            $this->authenticator->createAccount($values->email, $values->password, $values->name, $values->telephone);
+            $this->authenticator->createAccount($values->email, $values->password, $values->name, (int)$values->telephone);
             $this->presenter->flashMessage(
                 $this->translator->trans('flash.registrationSuccessful'),
                 FlashType::SUCCESS
