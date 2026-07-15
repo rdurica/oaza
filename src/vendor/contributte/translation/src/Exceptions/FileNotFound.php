490c5e525c08228dc19888c1db22c043b1c898db
Robert Durica <r.durica@gmail.com>
2026-07-15 22:42:29 +0200

    chore: stop tracking Composer vendor directory
    Remove src/vendor from git and ignore it so dependencies are installed via Composer only.

--- src/vendor/contributte/translation/src/Exceptions/FileNotFound.php
+++
@@ -1,8 +0,0 @@
-<?php declare(strict_types = 1);
-
-namespace Contributte\Translation\Exceptions;
-
-class FileNotFound extends Exception
-{
-
-}
--- src/vendor/ublaboo/datagrid/src/Column/ColumnText.php
+++
@@ -1,8 +0,0 @@
-<?php declare(strict_types = 1);
-
-namespace Contributte\Datagrid\Column;
-
-class ColumnText extends Column
-{
-
-}
